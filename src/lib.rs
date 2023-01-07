1169f4417e18bf289cf16069e5622cbaf8e2c3be
GC-BenLeeHughes <Ben.Hughes@gastongov.com>
2023-01-07 06:08:02 -0500

    Initial setup commits.

---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+/*!
+ # NQUSD
+ */
+
+ #![deny(
+    missing_docs,
+    nonstandard_style,
+    unused_variables,
+    unused_mut,
+    unused_parens,
+    unused_qualifications,
+    rust_2018_idioms,
+    rust_2018_compatibility,
+    future_incompatible,
+    missing_copy_implementations
+)]
+
+/// Simple test function to ensure this library is operational.
+#[inline]
+pub fn hello() {
+    println!("Hello, World!");
+}
